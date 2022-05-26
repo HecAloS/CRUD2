a57431c4c62ac6c7e6021b1b82367c5ac93ba0c4
HecAloS <hector.alosan.1@educa.jcyl.es>
2022-05-26 09:47:32 +0200

    Correcion Log4J

--- src/main/java/com/example/crud/rest/ProductoRest.java
+++ src/main/java/com/example/crud/rest/ProductoRest.java
@@ -3,8 +3,7 @@ package com.example.crud.rest;
 import java.util.List;
 import java.util.Optional;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -27,7 +26,8 @@ public class ProductoRest {
 	@Autowired
 	ProductoServicio productoServicio;
 	
-	static Logger log = LogManager.getLogger(ProductoRest.class);
+	private Logger log = Logger.getLogger(ProductoRest.class);
+	
 	
 	@PostMapping("/anadir")
 	private ResponseEntity<Producto> anadirProducto(@RequestBody Producto producto) {
